fd5acf5a3d655d49732ce27043ad85aff1d9dabd
astrecova <astretcovayamshikova@list.ru>
2020-12-28 19:58:32 +0300

    ..

--- test_run1.py
+++ test_run1.py
@@ -14,12 +14,15 @@ class TestMainPage1(object):
     def teardown_method(self):
         self.browser.quit()
 
-    @pytest.mark.parametrize('lincs', ["linc1", "linc2"])
-    def test_suite1(self, lincs):
-
-        lincs = ["https://stepik.org/lesson/236895/step/1",
-                 "https://stepik.org/lesson/236896/step/1"]
-        self.browser.get(lincs)
+    @pytest.mark.parametrize(
+        'link',
+        [
+            "https://stepik.org/lesson/236895/step/1",
+            "https://stepik.org/lesson/236896/step/1"
+        ]
+    )
+    def test_suite1(self, link):
+        self.browser.get(link)
         self.browser.implicitly_wait(15)
         input1 = self.browser.find_element_by_css_selector('.textarea')
 
@@ -28,7 +31,7 @@ class TestMainPage1(object):
 
         button = WebDriverWait(self.browser, 5).until(
             EC.element_to_be_clickable((By.CSS_SELECTOR, ".submit-submission"))
-    )
+        )
         button.click()
         self.browser.implicitly_wait(5)
         message = self.browser.find_element_by_css_selector(".smart-hints__hint")
